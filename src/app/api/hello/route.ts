59c2850845563223e2d6cd195c2f36a89b8653ca
Minung Han <hmu332233@gmail.com>
2023-03-13 01:33:43 +0900

    openai 코드 추가

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,17 @@
+import { NextResponse } from 'next/server';
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: '방금 한 말 영어로 번역해줘' }],
+  });
+
+  return NextResponse.json({ ...completion.data });
 }
